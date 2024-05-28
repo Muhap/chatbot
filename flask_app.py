468ac2b611282f34439eb79ecd91b8d67d077036
muhap ayman mahmoud <89155197+Muhap@users.noreply.github.com>
2024-05-28 04:14:53 +0300

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import query_data
+
+app = Flask(__name__)
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.get_json()
+    question = data.get('question')
+    response = query_data.main(question)
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ query_data.py
@@ -0,0 +1,50 @@
+import argparse
+from dataclasses import dataclass
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+you are professional ai chatbot fitness trainer ,Answer the question based only on the following context , answer with well explained response directly without intro :
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+if you feel like the questions doesn't related to the context ,then answer by your self.
+"""
+
+
+def main(question):
+    query_text = question
+
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    #print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    #print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main(question="how to avoid over training ?")
